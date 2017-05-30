50be81f3d70a3149f4a1389f00586e522eae3749
sangam <sangam.angre@gmail.com>
2017-05-30 16:45:42 +0530

    test cases created

--- build.py
+++ build.py
@@ -5,9 +5,3 @@ def solution(array):
     """
     Enter your code here
     """
-    F = np.array(array)
-    return 5*F/9 - 5*32/9
-
-faces = [0, 12, 45.21, 34, 99.91]
-res = solution(faces)
-print(res)
