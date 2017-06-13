3bdcfabaa47b7509fc7d92cc20c91fb0020c058e
mudassirkhan19 <mudassirkhan19@users.noreply.github.com>
2017-06-13 13:03:44 +0000

    Done

--- build.py
+++ build.py
@@ -6,3 +6,9 @@ def solution(array):
     """
     Enter your code here
     """
+    def to_celsius(far):
+        return ((float(far)-32)*5)/9
+
+    v_to_celsius = np.vectorize(to_celsius)
+
+    return v_to_celsius(array)
