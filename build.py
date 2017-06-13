c82266a92d3f6582c2f46d8977bee672d7934b03
ankanroy007 <ankanroy007@users.noreply.github.com>
2017-06-13 07:39:01 +0000

    Done

--- build.py
+++ build.py
@@ -6,3 +6,10 @@ def solution(array):
     """
     Enter your code here
     """
+    F = np.array(array)
+    celsius = (F - 32) * 5 / 9
+    return celsius
+
+
+
+solution([0,12,45.21,34.0,99.91])
