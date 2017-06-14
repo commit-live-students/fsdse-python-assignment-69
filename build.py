d5109c0a2f6819d27acc020c7c2bb81d308a201d
gyandhanee <gyandhanee@users.noreply.github.com>
2017-06-14 06:58:51 +0000

    Done

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
 import numpy as np
 
 
-
 def solution(array):
-    """
-    Enter your code here
-    """
+    newarray = []
+    for item in array:
+        newarray.append((item-32)*(5.0/9.0))
+    return newarray
