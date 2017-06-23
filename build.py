f9733de5a7607e7c96f19123d332aa84817a80d8
sagarambalam <sagar.ambalam@gmail.com>
2017-06-23 10:34:28 +0000

    Done

--- build.py
+++ build.py
@@ -3,6 +3,9 @@ import numpy as np
 
 
 def solution(array):
-    """
-    Enter your code here
-    """
+    list_f = array
+    list_c = []
+    for f in list_f:
+        c = (f - 32)*5./9
+        list_c.append(c)
+    return list_c
