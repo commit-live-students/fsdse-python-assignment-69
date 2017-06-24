6d372b09ec86e63cae4c332a24e37e7b9bb963a7
adinathauti <adinathauti@gmail.com>
2017-06-24 06:43:30 +0000

    Done

--- build.py
+++ build.py
@@ -3,6 +3,8 @@ import numpy as np
 
 
 def solution(array):
-    """
-    Enter your code here
-    """
+    ls = map(lambda x :((x-32)/1.8),array)
+    return ls
+
+
+solution([0, 12, 45.21, 34, 99.91])
